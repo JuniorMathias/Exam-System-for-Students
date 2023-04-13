5412f0383dbf9245f0887f78398ad7b5a7775975
junior <juniormathiias@gmail.com>
2023-04-13 00:09:24 -0300

    setting the database parte with some cors fixers

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ const port = process.env.PORT;
 
 const app = express();
 
-// config JSON and form data responses
 
+// config JSON and form data responses
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
+
+//Solve CORS 181
+
+app.use(cors({credentials: true, origin: "https://localhost:3000"}))
+
+//Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//DB CONNECTION
+require("./config/db.js");
+
+// routes
+const router = require("./routes/Router.js");
+app.use(router);
 
 app.listen(port, ()=>{
     console.log(`app listening on port ${port}`);
